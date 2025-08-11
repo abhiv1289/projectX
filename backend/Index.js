dfab37672c80b6873ebb2e4051321fa3f59998cd
Abhishek Vishwakarma <abhiv9255@gmail.com>
2025-08-11 18:26:13 +0530

    setting up error handlers

--- backend/Index.js
+++ backend/Index.js
@@ -2,6 +2,7 @@ import express, { json } from "express";
 import connectDB from "./config/database.js";
 import dotenv from "dotenv";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
 
@@ -9,8 +10,17 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT;
 
-app.use(express.json());
-app.use(cors());
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+
+app.use(
+  cors({
+    origin: process.env.ORIGIN_URL,
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 
 app.listen(port || 8000, () => {
