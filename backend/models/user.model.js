441cc6f46239841fcfbf5007fc6902d8797b9c95
Abhishek Vishwakarma <abhiv9255@gmail.com>
2025-08-13 16:22:50 +0530

    user and video models complete

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,29 +1,93 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const userSchema = new mongoose.Schema(
   {
     fullname: {
       type: String,
       required: true,
+      trim: true,
+      index: true,
     },
     username: {
       type: String,
       required: true,
+      lowercase: true,
+      trim: true,
+      unique: true,
+      index: true,
     },
     email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    avatar: {
+      type: String,
+      required: true,
+    },
+    coverImage: {
       type: String,
       required: true,
     },
     password: {
       type: String,
-      required: true,
+      required: [true, "Password is required"],
     },
+    refreshToken: {
+      type: String,
+    },
+    watchHistory: [
+      {
+        type: mongoose.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
   },
   {
     timestamps: true,
   }
 );
 
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(this.password, password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullname: this.fullname,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.ACCESS_REFRESH_SECRET,
+    {
+      expiresIn: process.env.ACCESS_REFRESH_EXPIRY,
+    }
+  );
+};
 const User = mongoose.model("User", userSchema);
 
 export default User;
