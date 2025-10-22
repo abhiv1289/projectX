78e132557885ca12dce4e2ca58ab1e1a32e6249c
Abhishek Vishwakarma <abhiv9255@gmail.com>
2025-10-22 17:08:33 +0530

    feat: Refactor server setup by removing Index.js and consolidating app.js with route handling

--- frontend/src/utility/axios.js
+++ frontend/src/utility/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
-      ? "http://localhost:8000/api"
+      ? "http://vystra-env.eba-tu6hymyc.ap-south-1.elasticbeanstalk.com/api"
       : "/api",
   withCredentials: true,
 });
