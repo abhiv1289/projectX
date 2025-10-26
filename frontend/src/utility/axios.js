380188bcf3778e63110e4ee75036249c84e00ece
Abhishek Vishwakarma <abhiv9255@gmail.com>
2025-10-26 13:07:11 +0530

    feat: Update axios base URL for production environment

--- frontend/src/utility/axios.js
+++ frontend/src/utility/axios.js
@@ -4,6 +4,6 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:8000/api"
-      : "http://vystra-env.eba-tu6hymyc.ap-south-1.elasticbeanstalk.com/api",
+      : "https://projectx-v5bn.onrender.com/api",
   withCredentials: true,
 });
