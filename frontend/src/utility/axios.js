c480f58a9ad7472d7bdecc4448aa430a1a657649
Abhishek Vishwakarma <abhiv9255@gmail.com>
2026-01-05 08:41:58 +0530

    fix: update axios baseURL to use the correct production API endpoint

--- frontend/src/utility/axios.js
+++ frontend/src/utility/axios.js
@@ -4,6 +4,6 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:8000/api"
-      : "https://projectx-v5bn.onrender.com/api",
+      : "https://vystra-backend.vercel.app/api",
   withCredentials: true,
 });
